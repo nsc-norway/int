4c28befad4496c26a762f19654754872bc0fce95
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2016-07-11 15:51:36 +0200

    Added sample and container importing

--- lims_import/import_package.py
+++ lims_import/import_package.py
@@ -2,6 +2,8 @@ from genologics.lims import *
 from genologics import config
 import json
 import re
+import sys
+import base64
 
 lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
 
@@ -12,6 +14,11 @@ class ProjectNameError(ValueError):
 def read_length(val):
     return int(re.match(r"(\d)+", val).group(0))
 
+def any_val(xs, default=None):
+    try:
+        return next(x for x in xs if x)
+    except StopIteration:
+        return default
 
 def get_project_fields(package):
 
@@ -32,33 +39,66 @@ def get_project_fields(package):
             'Method used to purify DNA/RNA': fields['purify_method'],
             'Method used to determine concentration': fields['concentration_method'],
             'Sample buffer': fields['buffer'],
-            'Sample prep requested': fields['rna_sample_preps'] or fields['dna_sample_prep'] or 'None',
+            'Sample prep requested': fields.get('rna_sample_preps') or fields.get('dna_sample_prep') or 'None',
             'Species': fields['species'],
             'Reference genome': fields['reference_genome'],
             'Sequencing method': fields['sequencing_type'],
             'Desired insert size': fields['insert_size'],
             'Sequencing instrument requested': fields['sequencing_instrument'],
-            'Read length requested': read_length(any(fields[f] for f in read_length_fields) or "0")
+            'Read length requested': read_length(any_val(fields[f] for f in read_length_fields, "0")),
+            'Index requested/used': files.get('index_seq')
             }
-
     return udfs
 
-
+def get_sample_fields(project_fields, sample):
+    udfs_raw = {
+            'Sample Type': 'TEST',
+            'Concentration': sample['conc'],
+            'A260/280': sample['a_260_280'],
+            'A260/230': sample['a_260_230'],
+            'Voume (ul)': sample['volume'],
+            'Total DNA/RNA (ug)': sample['total_dna_rna'],
+            'Primers, Linkers or RE sites present': sample['primers'],
+            'Approx no. reads Gb or lanes requested': sample['num_reads']
+            }
+    return dict(
+            (k, v)
+            for k, v in udfs_raw.items()
+            if v is not None
+            )
 
 def import_package(package_data):
     package = json.loads(package_data)
     project_name = package['fields']['project_name']
     apiuser = Researcher(lims, id="3") #APIUser
     projects = lims.get_projects(name=project_name)
-    if projects:
-        raise ProjectNameError("Project named " + str(project_name) + " already exists")
-    udfs = get_project_fields(package)
-    project = lims.create_project(name=project_name, researcher=apiuser, udf=udfs)
-    
-    
+    #if projects:
+    #    raise ProjectNameError("Project named " + str(project_name) + " already exists")
+    #udfs = get_project_fields(package)
+    #project = lims.create_project(name=project_name, researcher=apiuser, udf=udfs)
+    # ALTERNATIVE DEBUG:
+    project = projects[0]
 
+    container_names = set(sample['plate'] for sample in samples if sample['plate'])
+    containers = {}
+    if container_names:
+        plate96 = lims.get_container_types(name="96 well plate")[0]
+        for cn in container_names:
+            containers[cn] = lims.create_container(type=plate96, name=cn)
+    
+    samples = package['samples']
+    for sample in samples:
+        sample_udf = get_sample_fields(package['fields'], sample)
+        lims.create_sample('testmar', projects[0], containers.get(sample['plate']),
+                sample['position'], udf=sample_udf)
+
+    for f in package_data['files']:
+        gls = lims.glsstorage(project, f['filename'])
+        f_obj = gls.post()
+        data = base64.b64decode(f['content'])
+        f_obj.upload(data)
 
 if __name__ == "__main__":
-    data = open("testorder.json").read()
+    data = open(sys.argv[1]).read()
     import_package(data)
 
